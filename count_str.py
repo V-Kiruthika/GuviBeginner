666e91a7cff2cd4971daece43ce7168906834355
V-Kiruthika <53748912+V-Kiruthika@users.noreply.github.com>
2019-08-09 14:35:32 +0530

    Update count_str.py

--- count_str.py
+++ count_str.py
@@ -2,6 +2,6 @@ l=input()
 n=list(l)
 c=0
 for i in range(0,len(n)):
-    if (n[i].isalpha() or n[i].isdigit() or (ord(n[i])>=21 and ord(n[i])<=47)):
+    if (n[i].isalpha() or n[i].isdigit() or (ord(n[i])>=33 and ord(n[i])<=47)):
         c+=1
 print(c)
