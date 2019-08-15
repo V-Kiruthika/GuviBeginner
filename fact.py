3853c0dfd99460a3cbb735f2f07d66d8a1eece72
V-Kiruthika <53748912+V-Kiruthika@users.noreply.github.com>
2019-08-15 13:17:23 +0530

    Update fact.py

--- fact.py
+++ fact.py
@@ -1,10 +1,7 @@
 def fact(n):
-    if n==1 or n==0:
-        return 1
-    else:
-        f=1
-        for i in range(1,n+1):
-            f=f*i
-        return f
+    f=1
+    for i in range(1,n+1):
+        f=f*i
+    return f
 p=int(input())
 print(fact(p))
